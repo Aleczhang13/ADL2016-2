a3edb75540e66943f557af65a05ab2aa42726d7b
boboRAY <ray820821@gmail.com>
2016-12-12 15:02:18 +0800

    [hw4] fix script file 3

--- hw4/run_generation.sh
+++ hw4/run_generation.sh
@@ -1,3 +1,3 @@
-wget https://www.dropbox.com/s/7qtewjqgwnsfuwi/hw4_data.zip?dl=0 -0 hw4_data.zip
+wget -O hw4_data.zip https://www.dropbox.com/s/7qtewjqgwnsfuwi/hw4_data.zip?dl=0 
 unzip hw4_data.zip
 python predict_nlg.py $@
