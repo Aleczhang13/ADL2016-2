867211fc31d7108156e10fca4865d5b428e03b3b
boboRAY <ray820821@gmail.com>
2016-12-12 15:43:04 +0800

    [hw4] update shell script

--- hw4/run_generation.sh
+++ hw4/run_generation.sh
@@ -4,7 +4,7 @@ then
 	  echo "$file found."
 else
 	  echo "$file not found."
-    wget -O hw4_data.zip https://www.dropbox.com/s/7qtewjqgwnsfuwi/hw4_data.zip?dl=0
+    wget -O hw4_data.zip https://www.dropbox.com/s/xu1fei89vckjjlp/dl_hw4_data.zip?dl=0
 fi
 unzip -j hw4_data.zip
 python predict_nlg.py $@
