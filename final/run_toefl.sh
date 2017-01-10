fdcd71eb6ce90f2035e988d8491b72edaba9babf
boboRAY <ray820821@gmail.com>
2017-01-10 15:03:10 +0800

    [final] change shell script to use python3.5

--- final/run_toefl.sh
+++ final/run_toefl.sh
@@ -7,9 +7,9 @@ else
     wget -O dl_final_data.zip https://www.dropbox.com/s/ctheqrh8nllhzek/dl_final_squad_data.zip?dl=0
     unzip dl_final_data.zip
 fi
-pip3 install python-levenshtein tqdm --user
-python3 -m nltk.downloader -d $HOME/nltk_data punkt
-python3 pre_test.py $1
-python3 -m squad.prepro
-python3 -m basic.cli --len_opt --cluster --answer_path data/raw_ans.txt  --model_path model/toefl-model
-python3 includeWord.py data/raw_ans.txt $1 $2
+pip3.5 install python-levenshtein tqdm --user
+python3.5 -m nltk.downloader -d $HOME/nltk_data punkt
+python3.5 pre_test.py $1
+python3.5 -m squad.prepro
+python3.5 -m basic.cli --len_opt --cluster --answer_path data/raw_ans.txt  --model_path model/toefl-model
+python3.5 includeWord.py data/raw_ans.txt $1 $2
